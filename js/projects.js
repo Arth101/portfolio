a7b93e94a9d5a6a73c038b1c403d53339fc77a63
anders <anders@yacc.io>
2017-02-08 17:37:59 +0100

    vue and other new shit

---
+++ js/projects.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    projects: [
+      {
+        title: 'Harvikampfly.dk',
+        subtitle: 'Spare time project',
+        image: 'kampfly.jpg',
+        url: 'http://harvikampfly.dk/',
+        github: 'https://github.com/Arth101/harvikampfly.dk'
+      },
+      {
+        title: 'The 2nd Edit',
+        subtitle: 'Fashion Webshop',
+        image: 'the2ndedit.jpg',
+        url: 'https://www.the2ndedit.com/'
+      },
+      {
+        title: 'Mærkelex',
+        subtitle: 'Scout Badge catalog',
+        image: 'maerkelex.jpg',
+        url: 'http://xn--mrkelex-mxa.dk/',
+        github: 'https://github.com/hypesystem/maerkelex.dk'
+      },
+      {
+        title: 'Yacc.io homepage',
+        subtitle: 'Company Website',
+        image: 'yacc.jpg',
+        url: 'http://yacc.io/',
+        github: 'https://github.com/hypesystem/maerkelex.dk'
+      },
+    ]
+  }
+})
